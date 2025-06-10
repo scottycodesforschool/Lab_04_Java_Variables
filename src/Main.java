ad1eb8cfe0fdc801a8664b6e4e1a795d401424e4
Daniel <scott2dp@mail.uc.edu>
2025-06-10 11:19:33 +1000

    Updated with exercises

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,108 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args)
+    {
+        int intOperandA = 3;
+        int intOperandB = 7;
+        int intSum = 10;
+        int intDifference = 5;
+        int intProduct = 21;
+        int intQuotient = 4;
+        int intModulus = 3;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double doubleOperandA = 25.50;
+        double doubleOperandB = 7.21;
+        double doubleSum = 32.78;       // Will store the sum of doubleOperandA and doubleOperandB
+        double doubleProduct = 8.40;   // Will store the product
+        double doubleDifference = 18.30; // Will store the difference
+        double doubleQuotient = 7.80;  // Will store the quotient
+        double doubleModulo = 4.24;    // Will store the modulo
+
+        System.out.println("--- Integer Operations ---");
+
+        intSum = intOperandA + intOperandB; // Adding them together
+
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intDifference = intOperandA - intOperandB; // Subracting B from A
+
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+
+        intProduct = intOperandA * intOperandB; // Multiplying them together
+
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+        intQuotient = intOperandA / intOperandB; // Dividing A by B
+
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+
+        intModulus = intOperandA % intOperandB; // Modulus operator
+
+        System.out.println("The modulo of " + intOperandA + " and " + intOperandB + " is " + intModulus);
+
+        System.out.println("\n--- Double Operations ---");
+
+        doubleSum = doubleOperandA + doubleOperandB;
+
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+
+        doubleDifference = doubleOperandA - doubleOperandB;
+
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+
+        doubleProduct = doubleOperandA * doubleOperandB;
+
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+
+        doubleQuotient = doubleOperandA / doubleOperandB;
+
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+        doubleModulo = doubleOperandA % doubleOperandB;
+
+        System.out.println("The modulo of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo);
+
+        // The cost of my lunch
+        double myLunchCost = 18.50;
+
+        System.out.println("The cost of my lunch is: " + myLunchCost);
+
+        // The number of kids in your family
+        int numberOfKidsInFamily = 5;
+
+        System.out.println("The number of kids in my family is: " + numberOfKidsInFamily);
+
+        // The answer to is it raining?
+        boolean isItRaining = false;
+
+        System.out.println("Is it raining? " + isItRaining);
+
+        // The price of a gallon of gas
+        double priceOfGallonOfGas = 4.89; // Gas is expensive in Guam! Glad I got an EV :-)
+
+        System.out.println("The price of a gallon of gas is: " + priceOfGallonOfGas);
+
+        // Your favorite number
+        int favoriteNumber = 9;
+
+        System.out.println("My favorite number is: " + favoriteNumber);
+
+        // Your shoe size
+        double shoeSize = 10.5;
+
+        System.out.println("My shoe size is: " + shoeSize);
+
+        // Your birth month
+        String birthMonth = "February";
+
+        System.out.println("My birth month is: " + birthMonth);
+
+        // Your full name
+        String fullName = "Daniel Scott";
+
+        System.out.println("My full name is: " + fullName);
+
+
+
+
     }
 }
